1ddddad9286abf9b9becc87de061c3971dd76411
Arhum2 <arhumshahzad2003@gmail.com>
2022-06-28 02:39:27 -0400

    button

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,7 +1,10 @@
+import Button from './Button'
+
 const Header = ({title}) => {
   return (
-    <header>
-        <h1>{title}</h1>        
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='Add Task'/>
     </header>
   )
 }

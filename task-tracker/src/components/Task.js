b874d65caf310151bebbfbb99871f94699626ba3
Arhum2 <arhumshahzad2003@gmail.com>
2022-06-28 03:41:31 -0400

    reminder feature

--- task-tracker/src/components/Task.js
+++ task-tracker/src/components/Task.js
@@ -1,12 +1,12 @@
 import { FaTimes } from 'react-icons/fa'
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
+    <div className={`task ${task.reminder ? 'reminder' : ""}`} onDoubleClick={() => onToggle(task.id)}>
         <h3>{task.text}
         <FaTimes onClick={onDelete}/>
         </h3>
-        <p>{task.day}</p>
+        <p>{task.day} </p>
     </div>
   )
 }

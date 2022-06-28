d1cddc47c15b7c27a8d64658a3116b5254f97deb
Arhum2 <arhumshahzad2003@gmail.com>
2022-06-28 02:34:01 -0400

    styling

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,11 +1,12 @@
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello from React</h1>
+      <Header/>
     </div>
   );
 }
+
 
 export default App;

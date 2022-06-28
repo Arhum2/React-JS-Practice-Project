a801368a7512ebf56a1a542069d5bbe6788ed112
Arhum2 <arhumshahzad2003@gmail.com>
2022-06-28 03:25:11 -0400

    Task cell

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,9 +1,41 @@
 import Header from "./components/Header";
+import { useState } from "react"
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+
+    },
+    {
+        id: 2,
+        text: 'lunch',
+        day: 'Feb 10th at 5:30pm',
+        reminder: true,
+
+    },
+    {
+        id: 3,
+        text: 'Call',
+        day: 'Feb 11h at 7:30pm',
+        reminder: false,
+
+    },
+])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    console.log('del', id);
+  }
+
   return (
     <div className="container">
       <Header/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
